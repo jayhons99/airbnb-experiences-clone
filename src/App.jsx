4d70b4d7d425cb9c041aa7b4a709e380743b0a43
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-05-16 10:50:25 -0400

    added card with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Navbar from "./components/Navbar";
 import PhotoGrid from "./components/PhotoGrid";
+import katie from "./assets/katie-zaferes.png";
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
       <Navbar />
       <PhotoGrid />
+      <Card
+        img={katie}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,22 @@
+import star from "../assets/star.png";
+
+function Card(props) {
+  return (
+    <div className="--card">
+      <img src={props.img} className="--card-img"></img>
+      <div className="--card-metrics">
+        <img src={star} className="--card-star"></img>
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount})</span>
+        <span className="gray">• {props.country}</span>
+      </div>
+      <div className="--card-title">
+        <p>{props.title}</p>
+        <p>
+          <strong>From ${props.price} /</strong> person
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Card;
