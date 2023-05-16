c10611f03f7736348196abf4bf97d3ab9c772e0f
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-05-16 15:31:41 -0400

    add badges

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,16 @@
 import star from "../assets/star.png";
 
 function Card(props) {
+  let badge;
+  if (props.openSpots === 0) {
+    badge = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badge = "ONLINE";
+  }
   return (
     <div className="--card">
+      {badge && <div className="--card-badge">{badge}</div>}
+
       <img src={`/images/${props.img}`} className="--card-img"></img>
       <div className="--card-metrics">
         <img src={star} className="--card-star"></img>
